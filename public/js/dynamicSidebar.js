750d7c53ca7cff0e8e2693914b5cc3d01c1bc3d3
JanAdamowicz1 <adamowiczj01@gmail.com>
2024-01-16 15:06:10 +0100

    js script name change

---
+++ public/js/dynamicSidebar.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "90vw";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+function openCalendar() {
+    document.getElementById("sideCalendar").style.width = "90vw";
+}
+
+function closeCalendar() {
+    document.getElementById("sideCalendar").style.width = "0";
+}
